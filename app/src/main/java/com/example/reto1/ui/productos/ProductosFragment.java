89788161f54b7178412023d38c11ae0885b77920
Leonardo <jose.martinez.mt@correo.usa.edu.co>
2021-11-29 22:09:23 -0500

    first commit

---
+++ app/src/main/java/com/example/reto1/ui/productos/ProductosFragment.java
@@ -0,0 +1,8 @@
+package com.example.reto1.ui.productos;
+
+import androidx.fragment.app.Fragment;
+
+public class ProductosFragment extends Fragment {
+
+
+}
---
+++ app/src/main/java/com/example/reto1/ui/productos/ProductosModel.java
@@ -0,0 +1,8 @@
+package com.example.reto1.ui.productos;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProductosModel extends ViewModel {
+
+
+}
